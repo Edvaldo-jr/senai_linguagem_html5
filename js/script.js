3b5153beb850b74dec3dff521b2381f587d8b339
Edvaldo-Jr <edvaldo-oliveirajunior@hotmail.com>
2021-11-11 10:17:57 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector("#imagemBusca");
+
+btn.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    const imagem = document.querySelector("#name");
+
+    const value = imagem.value;
+
+alert ("Pesquise aqui");
+
+
+
+});
+    
